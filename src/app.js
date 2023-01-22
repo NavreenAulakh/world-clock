4052ab4311ab8329c13fc80fe7cd0cf7a663df56
NavreenAulakh <reenaaulakh123@gmail.com>
2023-01-21 22:00:14 -0500

    Added JS to display current time and date in chosen cities

---
+++ src/app.js
@@ -0,0 +1,31 @@
+//PARIS
+
+setInterval(function () {
+  let parisTime = moment().tz("Europe/Paris");
+
+  let parisElement = document.querySelector("#paris");
+  let parisDateElement = parisElement.querySelector(".date");
+  let parisTimeElement = parisElement.querySelector(".time");
+
+  parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
+  parisTimeElement.innerHTML = parisTime.format(
+    "h:mm:ss [<small>] A  [</small]"
+  );
+}, 1000);
+
+//Bangkok
+
+function setBankokTime() {
+  let bangkokTime = moment().tz("Asia/Bangkok");
+
+  let bangkokElement = document.querySelector("#bangkok");
+  let bangkokDateElement = bangkokElement.querySelector(".date");
+  let bangkokTimeElement = bangkokElement.querySelector(".time");
+
+  bangkokDateElement.innerHTML = bangkokTime.format("MMMM Do YYYY");
+  bangkokTimeElement.innerHTML = bangkokTime.format(
+    "h:mm:ss [<small>] A  [</small]"
+  );
+}
+
+setInterval(setBankokTime, 1000);
